106a67d801e933806614f259bc64e471e906cde0
MIGUEL SELLA PRADO <miguel.prado10@senaisp.edu.br>
2026-06-16 12:14:23 -0300

    atualizações

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,7 +1,29 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
-//design pattern: Singleton ---> permite  a criação de apenas uma instância da classe
+// Testa a conexão com o banco
+export async function testConnection() {
+    try {
+        const conn = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+
+        console.log('✅ Conectado com sucesso ao banco');
+
+        await conn.end();
+    } catch (err) {
+        console.error('❌ Erro ao conectar ao banco:', err);
+    }
+}
+
+// Singleton
 class Database {
     static #instance = null;
     #pool = null;
@@ -13,6 +35,11 @@ class Database {
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
             port: process.env.DB_PORT,
+
+            ssl: {
+                rejectUnauthorized: false
+            },
+
             waitForConnections: true,
             connectionLimit: 100,
             queueLimit: 0
@@ -24,90 +51,90 @@ class Database {
             Database.#instance = new Database();
             Database.#instance.#createPool();
         }
+
         return Database.#instance;
     }
+
     getPool() {
-        return this.#pool
+        return this.#pool;
     }
 }
 
 export const connection = Database.getInstance().getPool();
 
 export async function initializeDatabase() {
-    console.log("Inicializando o banco de dados e tabelas...");
+    console.log('Inicializando banco de dados...');
+
     try {
         const tempConnection = await mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
             port: process.env.DB_PORT,
-            ssl: { rejectUnauthorized: false }
+            ssl: {
+                rejectUnauthorized: false
+            }
         });
 
-
-        const dbName = process.env.DB_DATABASE || 'deploy';
-
-
-        await tempConnection.query(`CREATE DATABASE IF NOT EXISTS \`${dbName}\`;`);
-        await tempConnection.query(`USE \`${dbName}\`;`);
-
-        await tempConnection.query(
-            `
+        await tempConnection.query(`
             CREATE TABLE IF NOT EXISTS categorias(
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            nome VARCHAR(45) NOT NULL,
-            descricao VARCHAR(100) NULL,
-            dataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-            `);
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                nome VARCHAR(45) NOT NULL,
+                descricao VARCHAR(100) NULL,
+                dataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
 
         await tempConnection.query(`
             CREATE TABLE IF NOT EXISTS produtos (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            nome VARCHAR(45) NOT NULL,
-            descricao VARCHAR(45) NULL,
-            preco DECIMAL(10,2) NOT NULL,
-            image VARCHAR(250) NOT NULL,
-            quantidade DECIMAL (18,2) NOT NULL,
-            idCategoria INT NOT NULL,
-            dataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- 
-    
-            CONSTRAINT FK_produtos_categorias
-            FOREIGN KEY (idCategoria) REFERENCES categorias(id)
-        );
-            `)
-        await tempConnection.query(
-            `
-            CREATE TABLE IF NOT EXISTS pedidos(
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            valorTotal DECIMAL(10,2) NOT NULL,
-            Status ENUM("Aberto", "Finalizado", "Pendente") NOT NULL,
-            dataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-            `)
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                nome VARCHAR(45) NOT NULL,
+                descricao VARCHAR(45) NULL,
+                preco DECIMAL(10,2) NOT NULL,
+                image VARCHAR(250) NOT NULL,
+                quantidade DECIMAL(18,2) NOT NULL,
+                idCategoria INT NOT NULL,
+                dataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-        await tempConnection.query(
-            `
+                CONSTRAINT FK_produtos_categorias
+                FOREIGN KEY (idCategoria)
+                REFERENCES categorias(id)
+            );
+        `);
+
+        await tempConnection.query(`
+            CREATE TABLE IF NOT EXISTS pedidos(
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                valorTotal DECIMAL(10,2) NOT NULL,
+                Status ENUM('Aberto','Finalizado','Pendente') NOT NULL,
+                dataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+
+        await tempConnection.query(`
             CREATE TABLE IF NOT EXISTS itens_pedidos(
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            pedidoId INT NOT NULL,
-            produtoId INT NOT NULL,
-            quantidade DECIMAL(10,2) NOT NULL,
-            valorItem DECIMAL(10,2) NOT NULL,
-    
-            CONSTRAINT FK_itens_pedidos_pedidos
-            FOREIGN KEY (pedidoId) REFERENCES pedidos(id),
-    
-            CONSTRAINT FK_itens_pedidos_produtos
-            FOREIGN KEY (produtoId) REFERENCES produtos(id)
-        )
-            `)
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                pedidoId INT NOT NULL,
+                produtoId INT NOT NULL,
+                quantidade DECIMAL(10,2) NOT NULL,
+                valorItem DECIMAL(10,2) NOT NULL,
+
+                CONSTRAINT FK_itens_pedidos_pedidos
+                FOREIGN KEY (pedidoId)
+                REFERENCES pedidos(id),
+
+                CONSTRAINT FK_itens_pedidos_produtos
+                FOREIGN KEY (produtoId)
+                REFERENCES produtos(id)
+            );
+        `);
 
         await tempConnection.end();
-        console.log("Banco de dados e tabelas verificados/criados com sucesso.");
+
+        console.log('✅ Banco e tabelas verificados com sucesso');
     } catch (error) {
-        console.error("Erro ao criar o banco ou as tabelas:", error);
+        console.error('❌ Erro ao criar tabelas:', error);
         throw error;
     }
 }

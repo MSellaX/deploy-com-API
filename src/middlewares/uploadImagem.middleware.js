d48fe77b723f6e40e208cd20a63c58255efbd6af
MIGUEL SELLA PRADO <miguel.prado10@senaisp.edu.br>
2026-06-16 13:03:34 -0300

    atualizando

--- src/middlewares/uploadImagem.middleware.js
+++ src/middlewares/uploadImagem.middleware.js
@@ -1,29 +1,9 @@
 import createMulter from "../config/upload.Multer.js";
-import multer from "multer";
 
-const upload = createMulter({
+const uploadImage = createMulter({
     pasta: "imagens",
     tiposPermitidos: ["image/png", "image/jpeg"],
     tamanhoArquivo: 10 * 1024 * 1024 // 10MB
-}).any();
-
-const uploadImage = (req, res, next) => {
-    upload(req, res, (err) => {
-        if (err) {
-            console.error('Erro no upload de imagem:', err);
-            if (err instanceof multer.MulterError) {
-                return res.status(400).json({ message: err.message });
-            }
-            return res.status(400).json({ message: err.message || 'Erro no upload de imagem' });
-        }
-
-        // Normalizar para `req.file` usando o primeiro arquivo recebido (aceita nomes diferentes de campo)
-        if (!req.file && req.files && req.files.length > 0) {
-            req.file = req.files[0];
-        }
-
-        next();
-    });
-};
+}).single("image");
 
 export default uploadImage;
